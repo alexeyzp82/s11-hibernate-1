23bb86486b5a50902d4d266536d0a7752a23a3da
jasja1231 <jasja1231@gmail.com>
2022-10-15 18:07:15 +0300

    Added pattern to state name

--- src/main/java/com/softserve/itacademy/model/State.java
+++ src/main/java/com/softserve/itacademy/model/State.java
@@ -3,15 +3,20 @@ package com.softserve.itacademy.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="states")
 public class State {
 
     @Id
-    
     private int id;
 
+    @Size(min = 1, max = 20)
+    @NotBlank(message = "Name cannot be empty.")
+    @Pattern(regexp = "([A-Za-z0-9-_]+)")
     private String name;
 
     /**
