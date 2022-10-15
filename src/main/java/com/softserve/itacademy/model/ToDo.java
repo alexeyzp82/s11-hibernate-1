f3476b0f5ae060488f304298b8a58a428e5fb4f1
Max <mxcs02@gmail.com>
2022-10-15 16:20:07 +0300

    Add to-do model fields

--- src/main/java/com/softserve/itacademy/model/ToDo.java
+++ src/main/java/com/softserve/itacademy/model/ToDo.java
@@ -1,16 +1,43 @@
 package com.softserve.itacademy.model;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
-
+@Entity
+@Table(name = "todos")
 public class ToDo {
-//TODO
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @NotBlank
+    @Size(max = 255)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    @ManyToMany
+    @JoinTable(
+        name = "todo_collaborator",
+        joinColumns = @JoinColumn(name = "todo_id"),
+        inverseJoinColumns = @JoinColumn(name = "collaborator_id")
+    )
+    private List<User> collaborators;
+
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.ALL)
+    private List<Task> tasks;
 }
