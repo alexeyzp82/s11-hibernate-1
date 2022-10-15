a50f01338cead15dfc3977d717842248afa0dc11
jasja1231 <jasja1231@gmail.com>
2022-10-15 17:57:54 +0300

    states class essensials

--- src/main/java/com/softserve/itacademy/model/State.java
+++ src/main/java/com/softserve/itacademy/model/State.java
@@ -1,5 +1,29 @@
 package com.softserve.itacademy.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="states")
 public class State {
- //TODO
+
+    @Id
+    
+    private int id;
+
+    private String name;
+
+    /**
+     * Public constructor
+     * */
+    public State(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
