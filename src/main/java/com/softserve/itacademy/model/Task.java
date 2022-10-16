d378adadad8928ae8fcd64340d8cff3736f94cf1
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2022-10-16 09:55:18 +0300

    add Task

--- src/main/java/com/softserve/itacademy/model/Task.java
+++ src/main/java/com/softserve/itacademy/model/Task.java
@@ -1,5 +1,73 @@
 package com.softserve.itacademy.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+@Entity
+@Table(name = "tasks")
 public class Task {
-//
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotBlank
+    @NotNull
+    @Size(min = 3, max = 200)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @ManyToOne
+    @JoinColumn(name = "todo_id", nullable = false)
+    private ToDo toDo;
+
+    @OneToOne
+    @JoinColumn(name = "state_id", nullable = false)
+    private State state;
+
+    public Task() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public ToDo getToDo() {
+        return toDo;
+    }
+
+    public void setToDo(ToDo toDo) {
+        this.toDo = toDo;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
 }
